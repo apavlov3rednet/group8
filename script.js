1387956f96348a72da0421f7809205886ac07b5e
apavlov3rednet <92976265+apavlov3rednet@users.noreply.github.com>
2023-11-22 17:58:51 +0300

    form

--- script.js
+++ script.js
@@ -18,7 +18,12 @@
          * @returns 
          */
         getValue: function(key) {
-            return window.localStorage.getItem(key) || false;
+            let value = window.localStorage.getItem(key);
+
+            if(this.isJson(value))
+                return JSON.parse(value);
+
+            return value;
         },
 
         /** Удалить переменную
@@ -31,6 +36,140 @@
         /** Очистка базы */
         clear: function() {
             window.localStorage.clear();
+        },
+
+        isJson: function(value) {
+            try {
+                JSON.parse(value);
+            }
+            catch(error) {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    var DOM = {
+        /** Добавление элемента
+         * @param {*} tagName 
+         * @param {className, attrs{}, styles{}, children[], events{}} params 
+         * @returns 
+         */
+        create: function(tagName, params = {}) {
+            if(!tagName)
+                return false;
+
+            let element = document.createElement(tagName);
+
+            if(params.className) {
+                let classList = params.className.split(' ');
+                for(let i of classList) {
+                    element.classList.add(i);
+                }
+            }
+
+            if(typeof params.attrs === 'object') {
+                for(let index in params.attrs) {
+                    element.setAttribute(index, params.attrs[index]);
+                }
+            }
+
+            if(typeof params.styles === 'object') {
+                for(let i in params.styles) {
+                    element.style[i] = params.styles[i];
+                }
+            }
+
+            if(typeof params.events === 'object') {
+                for(let i in params.events) {
+                    element.addEventListener(i, params.events[i]);
+                }
+            }
+
+            if(typeof params.children === 'object' && params.children instanceof Array) {
+                params.children.forEach(item => {
+                    element.append(item);
+                });
+            }
+                
+            return element;
+        },
+
+        /** Изменение элемента
+         * @param {*} element 
+         * @param {*} params 
+         */
+        adjust: function(element = {}, params = {}) {
+            if(params.className) {
+                let classList = params.className.split(' ');
+                for(let i of classList) {
+                    element.classList.add(i);
+                }
+            }
+
+            if(typeof params.attrs === 'object') {
+                for(let index in params.attrs) {
+                    element.setAttribute(index, params.attrs[index]);
+                }
+            }
+
+            if(typeof params.styles === 'object') {
+                for(let i in params.styles) {
+                    element.style[i] = params.styles[i];
+                }
+            }
+
+            if(typeof params.events === 'object') {
+                for(let i in params.events) {
+                    element.addEventListener(i, params.events[i]);
+                }
+            }
+
+            if(typeof params.children === 'object' && params.children instanceof Array) {
+                params.children.forEach(item => {
+                    element.append(item);
+                });
+            }
+        },
+
+        /** Helper: Удаление всех стилей элемента
+         * @param {*} element 
+         */
+        removeStyles: function(element) {
+            element.removeAttribute('style');
+        },
+
+        /**
+         * 
+         * @param {*} element 
+         * @param {*} all 
+         */
+        clearItem: function(element = {}, all = false) {
+            element.innerHTML = '';
+
+            if(all) {
+                DOM.removeStyles(element);
+                element.removeEventListener();
+
+                // Array.from(element.attributes).forEach(item => {
+                //     element.removeAttribute(item);
+                // });
+            }
+        },
+
+        /**
+         * 
+         * @param {*} element 
+         * @param {*} total 
+         */
+        removeItem: function(element = {}, total = false) {
+            if(total) {
+                element.remove();
+            }
+            else {
+                DOM.clearItem(element, true);
+            }
         }
     }
 
@@ -77,11 +216,41 @@
          */
         getFullName: function() {
             if(Object.keys(this.userName).length != 0)
-                return this.userName.lastName + ' ' + this.userName.name;
+                return this.userName.lastName + ' ' + this.userName.name + this.getSecondName();
+        },
+
+        getSecondName: function() {
+            if(this.userName.secondName)
+                return ' ' + this.userName.secondName;
         }
     }
 
-    console.log(typeof Owner)
+    // let person = Owner;
+
+    // person.setName('Ivan', 'Petrov', 'Alekseevich');
+    // person.setBirthDate('11.11.2002');
+
+    // DataBase.setValue('user', person);
+
+    // let test = DOM.create('div', {
+    //     className: 'test some-class',
+    //     styles: {
+    //         width: '100px',
+    //         height: '100px',
+    //         backgroundColor: '#333',
+    //         display: 'block'
+    //     },
+    //     children: [
+    //         DOM.create('span')
+    //     ],
+    //     events: {
+    //         click: function() {
+    //             console.log('good');
+    //         }
+    //     }
+    // });
+
+    // document.body.append(test);
 
 })(window);
 

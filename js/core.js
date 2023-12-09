c959ed02b558762c2b5dba20857ed7d60ecb415f
apavlov3rednet <92976265+apavlov3rednet@users.noreply.github.com>
2023-12-09 17:54:21 +0300

    Оптимизация, формы, бренды

--- js/core.js
+++ js/core.js
@@ -2,61 +2,92 @@
     //Объекты разметки
     let obOwnerForm = document.getElementById('formOwner');
     let obCarForm = document.getElementById('formCar');
+    let obBrandForm = document.getElementById('formBrand');
+    let obModelForm = document.getElementById('formModel');
 
+    //Селекты
+    let selectOwner = obCarForm.querySelector('#owner');
+    let selectBrands = obCarForm.querySelector('#brand');
+    let selectModelBrands = obModelForm.querySelector('[name=BRAND]');
+    let selectModel = obCarForm.querySelector('#model');
+
+    //База данных
     let arOwner = DB.getValue('owners') || []; //массив базы даных
+    let arBrands = DB.getValue('brands') || [];
+    let arModel = DB.getValue('models') || [];
 
     //Метод обновления содержимого селекта владелец
-    function updateOwnerList() {
-        let selectOwner = obCarForm.querySelector('#owner');
+    function updateOwnerList(select, arr, title = 'Выберите') {
         let childrens = [];
 
-        selectOwner.innerHTML = '';
+        select.innerHTML = '';
 
         //Дефолтная позиция
         childrens.push(DOM.create('option', {
             attrs: { value: 0},
-            text: 'Выбери'
+            text: title
         }));
 
         //Собираем список валдельцев в потомки селекта
-        arOwner.forEach(item => {
+        arr.forEach(item => {
             childrens.push(DOM.create('option', {
                 attrs: { value: item.id },
-                text: item.params.LAST_NAME + ' ' + item.params.NAME
+                text: Object.values(item.params).join(' ') //Объединение всех значений через пробел
             }));
         });
 
         //Обновляем селект
-        DOM.adjust(selectOwner, {
+        DOM.adjust(select, {
             children: childrens
         });
     }
 
-    //Обрабатываем отправку формы
-    obOwnerForm.addEventListener('submit', function(event) {
-        event.preventDefault(); //Отменяем штатное поведение события
+    function bindEvents(obForm, ar, db, callback = []) {
+        obForm.addEventListener('submit', function(event) {
+            event.preventDefault();
 
-        let arFields = obOwnerForm.querySelectorAll('input');
+            let arFields = obForm.querySelectorAll('input, select');
 
-        //Создаем модель формы
-        let owner = new Model();
+            let model = new Model();
 
-        arFields.forEach(item => {
-            let params = {};
-            params[item.name] = item.value;
-            owner.set(params);
+            arFields.forEach(item => {
+                let params = {};
+                params[item.name] = item.value;
+                model.set(params);
+                item.value = '';
+            });
+
+            ar.push(model);
+
+            //Добавляем в базу данных
+            DB.setValue(db, ar);
+
+            callback.forEach(item => {
+                updateOwnerList(item, ar);
+            });
         });
+    }
 
-        arOwner.push(owner);
+    selectBrands.addEventListener('change', function(event) {
+        event.preventDefault();
 
-        //Добавляем в базу данных
-        DB.setValue('owners', arOwner);
+        let value = selectBrands.value;
 
-        //Вызываем обновление селекта
-        updateOwnerList();
+        if(arModel.length > 0) {
+            let newAr = arModel.filter(item => item.params.BRAND === value);
+            updateOwnerList(selectModel, newAr);
+        }
     });
 
-     //Вызываем обновление селекта
-    updateOwnerList();
+    //Навешивание событий на формы
+    bindEvents(obOwnerForm, arOwner, 'owners', [selectOwner]);
+    bindEvents(obBrandForm, arBrands, 'brands', [selectBrands, selectModelBrands]);
+    bindEvents(obModelForm, arModel, 'models', [selectModel]);
+
+    //Вызываем обновление селекта
+    updateOwnerList(selectOwner, arOwner, 'Выберите владельца');
+    updateOwnerList(selectBrands, arBrands);
+    updateOwnerList(selectModelBrands, arBrands);
+    updateOwnerList(selectModel, arModel);
 
 })(window);
